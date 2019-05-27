f0b486c81852ca11b71cdf7a8c0aa80749ac8e72
rawlmp <lokoconbotas@gmail.com>
2019-05-27 16:43:58 +0200

    First node app deploy

---
+++ front.js
@@ -0,0 +1,104 @@
+const socket = io();
+const d = document;
+
+//Colocar el reloj en el html...
+const deviceTime = d.querySelector(".status-bar .time");
+const messageTime = d.querySelectorAll(".message .time");
+
+deviceTime.innerHTML = moment().format("LT");
+
+// ... y actualizarlo cada segundo usando la libreria moment.js
+setInterval(function() {
+  deviceTime.innerHTML = moment().format("LT");
+}, 1000);
+
+//Los "listeners" de socket a los emit del server.js
+socket.on("message", addMessages);
+socket.on("user", updateUsers);
+socket.on("exit", updateUsers);
+
+//Eventos en los botones
+d.querySelector("#send").onclick = () => {
+  sendMessage({
+    name: d.querySelector("#name").value || "Guest",
+    message: d.querySelector("#message").value,
+    time: moment().format("LT")
+  });
+};
+
+d.querySelector("#message").onkeyup = e => {
+  if (e.keyCode === 13 && d.querySelector("#message").value.trim().length > 0) {
+    sendMessage({
+      name: d.querySelector("#name").value || "Guest",
+      message: d.querySelector("#message").value,
+      time: moment().format("LT")
+    });
+  }
+};
+
+d.querySelector("#enter").onclick = () => {
+  d.querySelector(".user").style.display = "none";
+  d.querySelector(".noUser").style.display = "";
+  d.querySelector(".noUser2").style.display = "";
+  getMessages();
+};
+
+//Funciones
+function updateUsers(num) {
+  d.querySelector(".status").innerHTML = num + " miembros online";
+}
+
+function addMessages(message) {
+  if (message.name == d.querySelector("#name").value) {
+    d.querySelector("#messages").insertAdjacentHTML(
+      "beforeend",
+      `
+    <div class="message sent">
+      ${message.message}
+      <span class="metadata">
+        <span class="time">${message.time}</span>
+        <span class="tick">
+          <i class="zmdi zmdi-check-all"></i>
+        </span>
+      </span>
+    </div>
+    `
+    );
+  } else {
+    d.querySelector("#messages").insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="message received">
+        <p class="nombre">${message.name}</p>
+        <span>${message.message}</span>
+        <span class="metadata">
+        <span class="time">${message.time}</span>
+        <span class="tick">
+          <i class="zmdi zmdi-check-all"></i>
+        </span>
+      </span>
+      </div>
+    `
+    );
+  }
+  d.querySelector("#messages").scrollTop = d.querySelector(
+    "#messages"
+  ).scrollHeight;
+}
+
+async function getMessages() {
+  let response = await fetch("http://localhost:3000/messages");
+  let data = await response.json();
+  data.forEach(addMessages);
+}
+
+function sendMessage(message) {
+  fetch("http://localhost:3000/messages", {
+    method: "POST",
+    body: JSON.stringify(message),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+  d.querySelector("#message").value = "";
+}
---
+++ server.js
@@ -0,0 +1,67 @@
+//Express es un framework web para Node
+var express = require("express");
+var app = express();
+var server = app.listen(3000);
+
+//Estas opciones son requisito para usar los datos en el POST y en el GET.
+//Antes se usaba una librería llamada BodyParser. En la última versión de Express >4.0
+//ya no es necesario
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Esta instrucción es para que el propio JS cargue el HTML
+app.use(express.static(__dirname));
+
+//Socket.io es el encargado de gestionar los websockets.
+//Estos websockets son como "listeners" y "fetch" a la vez...
+var io = require("socket.io")(server);
+
+//Mongoose es el encargado de gestionar la base de datos de MongoDB
+var mongoose = require("mongoose");
+
+//Url de la database en MLAB
+var dbUrl =
+  "mongodb+srv://raul:ubiqum2019@mongochat-q3fmc.mongodb.net/test?retryWrites=true";
+
+//Conectamos con la BBDD
+mongoose.connect(dbUrl, { useNewUrlParser: true }, err => {
+  console.log("mongodb connected");
+});
+
+//Esquema o modelo del mensaje
+var Message = mongoose.model("Message", {
+  name: String,
+  message: String,
+  time: String
+});
+
+//Función ejecutada al usar enviar un GET al endpoint /messages
+app.get("/messages", (req, res) => {
+  Message.find({}, (err, messages) => {
+    res.send(messages);
+  });
+});
+
+//Función ejecutada al usar enviar un POST al endpoint /messages
+app.post("/messages", (req, res) => {
+  var message = new Message(req.body);
+  message.save(err => {
+    if (err) sendStatus(500);
+    //Emitimos un evento "message cuando se envía un POST"
+    io.emit("message", req.body);
+    res.sendStatus(200);
+  });
+});
+
+//Al conectarse un usuario se le une a la sala Ubiqum y se emite el evento "user"
+io.on("connection", socket => {
+  socket.join("ubiqum");
+  io.emit("user", io.sockets.adapter.rooms["ubiqum"].length);
+
+  //Al desconectar se dispara el evento "exit"
+  socket.on("disconnect", () => {
+    if (io.sockets.adapter.rooms["ubiqum"]) {
+      io.emit("exit", io.sockets.adapter.rooms["ubiqum"].length);
+    }
+  });
+});
